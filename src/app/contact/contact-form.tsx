6bc02b04e57931eff78b982bb86514133946237f
rubik proxy <rubikproxy@gmail.com>
2025-07-06 12:11:01 +0000

    Hydration failed because the server rendered HTML didn't match the clien

--- src/app/contact/contact-form.tsx
+++ src/app/contact/contact-form.tsx
@@ -118,7 +118,7 @@ export function ContactForm() {
                 <FormItem>
                   <FormLabel>Full Name</FormLabel>
                   <FormControl>
-                    <Input placeholder="Your Name" {...field} autoComplete="name" />
+                    <Input type="text" placeholder="Your Name" {...field} autoComplete="name" />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
